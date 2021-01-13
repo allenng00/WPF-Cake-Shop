3faa6e0049906ca2d0def028a18a8cda6ced7d05
truongtrieu123 <uerittruong@gmail.com>
2021-01-13 17:22:58 +0700

    Add DateTime Converter

--- SourceCode/CakeShop/Converters.cs
+++ SourceCode/CakeShop/Converters.cs
@@ -24,6 +24,20 @@ namespace CakeShop
         }
     }
 
+    class DateTimeToSpecifiedStyleString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dateTime = (DateTime)value;
+            return dateTime.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     /// <summary>
     /// Chuyển đổi đường dẫn tuyệt đối sang tương đối
     /// </summary>
